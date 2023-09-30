5582e1784216bcfd3d5f6e99820e9689d72327d1
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-09-30 09:51:09 +0200

    feat: :tada: add config and current shell parsing

---
+++ src/config.rs
@@ -0,0 +1,118 @@
+use core::fmt;
+use std::{convert, ffi::OsStr, fs, io, path::Path, str::FromStr};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct Config {
+    pub aliases: Vec<Alias>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct Alias {
+    pub alias: String,
+    pub equals: String,
+    pub shells: Option<ShellList>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+#[serde(rename_all = "lowercase")]
+pub enum ShellList {
+    Blacklist(Vec<Shell>),
+    Whitelist(Vec<Shell>),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+#[serde(rename_all = "lowercase")]
+pub enum Shell {
+    Bash,
+    Fish,
+}
+
+pub enum ShellParsingError {
+    UnknownShell(String),
+    BorkedOsStr,
+    NoFileName,
+}
+
+impl fmt::Display for ShellParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ShellParsingError::UnknownShell(unknown_shell) => {
+                write!(f, "Unknown shell: {}", unknown_shell)
+            }
+            ShellParsingError::NoFileName => write!(f, "Could not get file name from shell path"),
+            ShellParsingError::BorkedOsStr => write!(f, "Could not convert shell OsStr to &str"),
+        }
+    }
+}
+
+impl FromStr for Shell {
+    type Err = ShellParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path = Path::new(s);
+
+        let os_file_name: &OsStr;
+        if let Some(f) = path.file_name() {
+            os_file_name = f;
+        } else {
+            return Err(ShellParsingError::NoFileName);
+        }
+
+        let file_name: &str;
+        if let Some(f) = os_file_name.to_str() {
+            file_name = f
+        } else {
+            return Err(ShellParsingError::BorkedOsStr);
+        }
+
+        match file_name {
+            "bash" => Ok(Shell::Bash),
+            "fish" => Ok(Shell::Fish),
+            unknown => Err(ShellParsingError::UnknownShell(unknown.to_string())),
+        }
+    }
+}
+
+pub enum ConfigError {
+    Io(io::Error),
+    Deserialization(serde_json::Error),
+}
+
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ConfigError::Io(err) => write!(f, "I/O error: {}", err),
+            ConfigError::Deserialization(err) => {
+                write!(f, "Error in JSON config: {}", err)
+            }
+        }
+    }
+}
+
+impl convert::From<io::Error> for ConfigError {
+    fn from(err: io::Error) -> ConfigError {
+        return ConfigError::Io(err);
+    }
+}
+
+impl convert::From<serde_json::Error> for ConfigError {
+    fn from(err: serde_json::Error) -> ConfigError {
+        return ConfigError::Deserialization(err);
+    }
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> Result<Self, ConfigError> {
+        let path = Path::new(path);
+        let file_content = fs::read_to_string(&path)?;
+        let config: Config = serde_json::from_str(&file_content)?;
+
+        return Ok(config);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+pub mod config;
+
+use std::{env, process::exit, str::FromStr};
+
+use config::Config;
+
+use argparse::{ArgumentParser, StoreOption};
+
+use crate::config::Shell;
+
+const DEFAULT_CONFIG_PATH: &str = ".config/alias-rs/config.json";
+
+fn failure(msg: String) -> ! {
+    eprintln!("Failure: {}", msg);
+    exit(1)
+}
+
+fn main() {
+    let mut config_path: Option<String> = None;
+    let config_option_help = format!(
+        "Path to the configuration file (default: $HOME/{})",
+        DEFAULT_CONFIG_PATH
+    );
+    let mut shell: Option<Shell> = None;
+    let shell_option_help = format!(
+        "Shell to create aliases for (default: $SHELL/{})",
+        DEFAULT_CONFIG_PATH
+    );
+
+    {
+        let mut ap = ArgumentParser::new();
+
+        ap.refer(&mut config_path).add_option(
+            &["-c", "--config"],
+            StoreOption,
+            &config_option_help,
+        );
+
+        ap.refer(&mut shell)
+            .add_option(&["-s", "--shell"], StoreOption, &shell_option_help);
+
+        ap.parse_args_or_exit();
+    }
+
+    let config_path = match config_path {
+        Some(c) => c,
+        None => match env::var("HOME") {
+            Ok(h) => format!("{}/{}", h, DEFAULT_CONFIG_PATH),
+            Err(_) => failure("Could not obtain default config path".to_string()),
+        },
+    };
+
+    let config = match Config::from_file(config_path.as_str()) {
+        Ok(c) => c,
+        Err(e) => failure(format!("Could not obtain config: {}", e)),
+    };
+
+    let shell = match shell {
+        Some(s) => s,
+        None => match env::var("SHELL") {
+            Ok(s) => match Shell::from_str(s.as_str()) {
+                Ok(s) => s,
+                Err(e) => failure(format!("Could not obtain default shell: {}", e)),
+            },
+            Err(e) => failure(format!("Could not obtain default shell: {}", e)),
+        },
+    };
+
+    println!("{:?}, {:?}", config, shell);
+}
